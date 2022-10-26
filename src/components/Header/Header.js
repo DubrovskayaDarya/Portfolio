5be49f1b383801915f3330d4b788b79d1a7bbe17
DubrovskayaDaya <dashadubrovskaya1999@gmail.com>
2022-10-26 15:03:49 +0300

    fix: nav links in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import style from './Header.module.scss';
-import {HashLink, HashLink as Link} from "react-router-hash-link";
 
 
 export const Header = () => {
 
+    const onClickHandler = (id) => {
+        const element = document.getElementById(id)
+        element?.scrollIntoView({block: "start", behavior: 'smooth'})
+    }
+
     return (
         <header className={style.header}>
             <div className={style.nav}>
-                <HashLink to={'Portfolio/#main'}> Main </HashLink>
-                <Link to={'/#skills'}> Skills </Link>
-                <Link to={'/#projects'}> Projects </Link>
-                <Link to={'/#contacts'}> Contacts </Link>
+                <a onClick={() => onClickHandler("main")}>Main</a>
+                <a onClick={() => onClickHandler("projects")}>Projects</a>
+                <a onClick={() => onClickHandler("skills")}>Skills</a>
+                <a onClick={() => onClickHandler("contacts")}>Contacts</a>
             </div>
         </header>
     );
