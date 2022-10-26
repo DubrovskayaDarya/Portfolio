feb725c5d43030f8dbc61a855638cb8959f48b37
DubrovskayaDaya <dashadubrovskaya1999@gmail.com>
2022-10-26 14:48:28 +0300

    test: link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './Header.module.scss';
-import {HashLink as Link} from "react-router-hash-link";
+import {HashLink, HashLink as Link} from "react-router-hash-link";
 
 
 export const Header = () => {
@@ -8,7 +8,7 @@ export const Header = () => {
     return (
         <header className={style.header}>
             <div className={style.nav}>
-                <Link to={'/#main'}> Main </Link>
+                <HashLink to={'/#main'}> Main </HashLink>
                 <Link to={'/#skills'}> Skills </Link>
                 <Link to={'/#projects'}> Projects </Link>
                 <Link to={'/#contacts'}> Contacts </Link>
